2c8615ed14db1458f65bae5be8f0b571347c3a18
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2024-08-26 18:02:28 -0500

    feat: Scaffold Example

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +24,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab04Theme {
-                
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldExample() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Scaffold Example") }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /* Acción */ }) {
+                Text("+")
+            }
+        },
+        content = { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                Text("Content goes here", modifier = Modifier.padding(16.dp))
+            }
+        }
+        )
+    }
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab04Theme {
+        ScaffoldExample()
+    }
+}
